edbbce6a30a990dc7dcebbc72e9f7bd546af9331
vadikonline1 <vadikonline1@gmail.com>
2026-01-09 09:55:19 +0200

    Update custom-social-fields-shortcodes.php

--- custom-social-fields-shortcodes.php
+++ custom-social-fields-shortcodes.php
@@ -7,7 +7,8 @@
  * Author: Steel..xD
  * License: GPL v2 or later
  * Text Domain: scfs-oop
-*/
+ * Domain Path: /languages
+ */
 
 if (!defined('ABSPATH')) exit;
 
@@ -37,5 +38,41 @@ add_action('plugins_loaded', function() {
     SCFS\Frontend::get_instance();
 });
 
-// Hook pentru crearea tabelei la activarea plugin-ului
 register_activation_hook(__FILE__, ['SCFS\\AjaxHandler', 'create_database_table_on_activation']);
+
+// Hook pentru activare
+register_activation_hook(__FILE__, ['SCFS\\Activator', 'activate']);
+
+// Hook pentru dezactivare
+register_deactivation_hook(__FILE__, ['SCFS\\Activator', 'deactivate']);
+
+// Hook pentru dezinstalare
+register_uninstall_hook(__FILE__, ['SCFS\\Uninstaller', 'uninstall']);
+
+// Funcție pentru a obține instanța principală a plugin-ului (pentru compatibilitate)
+function scfs() {
+    return SCFS\Main::get_instance();
+}
+
+// Adaugă link-uri pentru donații/recenzii (opțional)
+add_filter('plugin_row_meta', function($links, $file) {
+    if (plugin_basename(__FILE__) === $file) {
+        $row_meta = [
+            'docs' => '<a href="https://github.com/vadikonline1/custom-fields-shortcodes/wiki" target="_blank">Documentation</a>',
+            'support' => '<a href="https://github.com/vadikonline1/custom-fields-shortcodes/issues" target="_blank">Support</a>',
+        ];
+        
+        return array_merge($links, $row_meta);
+    }
+    
+    return $links;
+}, 10, 2);
+
+// Funcție pentru compatibilitate cu cod vechi (dacă era folosită clasa SCFS_Main_Plugin)
+if (!class_exists('SCFS_Main_Plugin')) {
+    class SCFS_Main_Plugin {
+        public static function get_instance() {
+            return SCFS\Main::get_instance();
+        }
+    }
+}
