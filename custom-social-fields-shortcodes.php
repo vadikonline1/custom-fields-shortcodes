90acb2527df5992fe2af4e7123f0eb64603a89f2
vadikonline1 <vadikonline1@gmail.com>
2026-04-22 15:29:58 +0300

    Bump version to 1.3.4
    css + js

--- custom-social-fields-shortcodes.php
+++ custom-social-fields-shortcodes.php
@@ -3,7 +3,7 @@
  * Plugin Name: Social & Custom Fields Shortcodes
  * Plugin URI: https://github.com/vadikonline1/custom-fields-shortcodes
  * Description: Manage custom fields and social floating buttons with shortcodes.
- * Version: 1.3.3
+ * Version: 1.3.4
  * Author: Steel..xD
  * License: GPL v2 or later
  * Text Domain: scfs-oop
